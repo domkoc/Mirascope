ea957be281566646b74aa966763ff0402dd29190
Dominik Kocka <kocka98@yahoo.com>
2020-11-04 19:28:28 +0100

    Initial commit 2.0

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -18,8 +18,8 @@
 //
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
-// Nev    : Filip Krisztina
-// Neptun : QE4L0M
+// Nev    : Kocka Dominik
+// Neptun : FIBRPN
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
 // mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
